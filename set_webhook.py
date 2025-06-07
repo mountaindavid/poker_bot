f6a94cabc3ab56bc65c5f9b18ed27be1b9efcda7
David <16david93@gmail.com>
2025-06-07 13:15:12 -0700

    set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-RAILWAY_URL = "https://your-app-name.up.railway.app"  # без слэша в конце
+RAILWAY_URL = os.getenv("WEBHOOK_URL")
 
 url = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={RAILWAY_URL}/{TOKEN}"
 print(requests.get(url).json())
