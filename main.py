e4f75ca56027eb797ea80b22d2b45985abe882cf
David <16david93@gmail.com>
2025-06-07 14:04:42 -0700

    token fix

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import os
 import telebot
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+WEBHOOK_SECRET_PATH = os.getenv("WEBHOOK_SECRET_PATH", "supersecret")
+
 app = Flask(__name__)
 
-@app.route(f"/{TOKEN}", methods=['POST'])
+@app.route(f"/{WEBHOOK_SECRET_PATH}", methods=['POST'])
 def webhook():
     json_str = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json_str)
@@ -18,8 +20,7 @@ def index():
     return "Poker Bot is alive!", 200
 
 if __name__ == '__main__':
-    from os import getenv
-    port = int(getenv("PORT", 8443))
-    bot.remove_webhook()  # Удаляем старый вебхук, если есть
-    bot.set_webhook(url=f'{getenv("WEBHOOK_URL")}/{TOKEN}')
+    port = int(os.getenv("PORT", 8443))
+    bot.remove_webhook()
+    bot.set_webhook(url=f'{os.getenv("WEBHOOK_URL")}/{WEBHOOK_SECRET_PATH}')
     app.run(host='0.0.0.0', port=port)
